5b675379bc126d8e8f9cc38f4c1fa2c6cd793ff8
dkulyk <dkulyk@foundationmedicine.com>
2023-10-12 16:54:22 +0300

    Install fontawesome

--- Print3dMarketplace.UI/src/app/app.module.ts
+++ Print3dMarketplace.UI/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RoleSelectionComponent } from './components/role-selection/role-selection.component';
+import { CustomerRegistrationComponent } from './components/customer-registration/customer-registration.component';
+import { CreatorRegistrationComponent } from './components/creator-registration/creator-registration.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoleSelectionComponent,
+    CustomerRegistrationComponent,
+    CreatorRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent, FontAwesomeModule]
 })
 export class AppModule { }
