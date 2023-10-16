4fa6f91a72da2712091e994e6e86662e274c83c9
dkulyk <dkulyk@foundationmedicine.com>
2023-10-16 10:47:10 +0300

    Update creators and customers registration

--- Print3dMarketplace.UI/src/app/components/creator-registration/creator-registration.component.ts
+++ Print3dMarketplace.UI/src/app/components/creator-registration/creator-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faMapMarker, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircleArrowRight, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'creator-registration',
@@ -6,4 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./creator-registration.component.css']
 })
 export class CreatorRegistrationComponent {
+  faUser = faUser;
+  faEnvelope = faEnvelope;
+  faLock = faLock;
+  faCircleArrowRight = faCircleArrowRight;
+  faMapMarker = faMapMarker;
+  faPhone = faPhone;
 }
